4b71b55ad04967bbadf29064bb6b7f38c95f8139
ArieBennett <mr.bennett1995@hotmail.com>
2020-12-13 22:17:07 +1030

    update chip8 files w/ skeleton

--- chip8.cpp
+++ chip8.cpp
@@ -1,11 +1,37 @@
 #include "chip8.h"
 
+#define carryFlag V[15]
+#define spriteCollision V[15]
+
 using namespace std;
 
-chip8::chip8() {
-	//
+// initialize registers & memory
+void chip8::initialize() {
+	return;
 }
 
-chip8::~chip8() {
-	//
+void chip8::loadGame(string gameName) {
+	return;
+}
+
+
+void chip8::emulateCycle() {
+	// fetch opcode
+
+	// decode opcode
+
+	// execute opcode
+
+	// update timers
+
+
+	return;
+}
+
+void chip8::drawGraphics() {
+	return;
+}
+
+void chip8::setKeys() {
+	return;
 }
--- chip8.h
+++ chip8.h
@@ -1,10 +1,33 @@
 #ifndef CHIP8_H
 #define CHIP8_H
 
+#include <string>
+
 class chip8 {
-	public:
-		chip8();
-		~chip8();
+    // memory variables
+    unsigned char memory[4096];     // 4k-bit block of memory
+    unsigned char V[16];            // 16 8-bit CPU registers; V0..VE, VF is flag for arithmetic carry / sprite collision
+    unsigned short opcode;          // 16-bit currently loaded opcode
+    unsigned short I;               // address register
+    unsigned short pc;              // program counter; 0x000..0xFFF
+    unsigned short stack[16];       // the stack w/ 16 levels
+    unsigned short sp;              // stack pointer
+
+    // i/o variables & timers
+    unsigned char gfx[64*32];       // 2048 b/w pixels
+    unsigned char key[16];          // keypad; 0x0..0xF
+    unsigned char delay_timer;      // 60Hz timer
+    unsigned char sound_timer;      // 60Hz timer; counts 60..0, sounds when hits 0
+
+    // other variables
+    unsigned char drawFlag;         // controls whether to update the screen or not
+
+    public:
+        void initialize();
+        void loadGame(std::string);
+        void emulateCycle();
+        void drawGraphics();
+        void setKeys();
 };
 
 #endif

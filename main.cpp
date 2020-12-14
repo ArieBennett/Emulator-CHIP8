cb488ce6a5a462ef27e1482f00ccc58004da87b4
ArieBennett <mr.bennett1995@hotmail.com>
2020-12-14 12:19:09 +1030

    Update main.cpp
    Setup program flow

--- main.cpp
+++ main.cpp
@@ -1,7 +1,50 @@
+#include <SDL2/SDL.h>			// initializing and shutdown functions
+#include <SDL2/SDL_image.h>		// rendering images and graphics on screen
+#include <SDL2/SDL_timer.h> 	// SDL_Delay() functions
+#include <string>
 #include "chip8.h"
 
 using namespace std;
 
+// variables
+chip8 myChip8;
+string gameName;
+
+// define functions
+void setupGraphics();
+void setupInput();
+void drawGraphics();
+
 int main(int argc, char **argv) {
+	// setup
+	setupGraphics();
+	setupInput();
+
+	// initialize chip8 system
+	myChip8.initialize();
+	myChip8.loadGame(gameName);
+
+	// emulation loop
+	while(true) {
+		myChip8.emulateCycle();
+
+		if(myChip8.drawFlag)
+			drawGraphics();
+
+		myChip8.setKeys();
+	}
+
 	return 0;
+}
+
+void setupGraphics() {
+
+}
+
+void setupGraphics() {
+	
+}
+
+void drawGraphics() {
+
 }
